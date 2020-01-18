4619a559a6bdf0414ffc643a63a8b481d1e5bd2f
renan-alves <renan.a.a.costa@gmail.com>
2020-01-17 23:36:42 -0300

    Inicio do Content

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Profile from './Profile';
+import Content from './Content';
 
 export default class App extends React.Component {
 
@@ -9,6 +10,7 @@ export default class App extends React.Component {
             <div>
                 <Header />
                 <Profile />
+                <Content />
             </div>
         );
     }
---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Content extends React.Component{
+
+    render(){
+
+        return(
+            <div className="content">
+                <div>Formação</div>
+                <div>Competências</div>
+                <div>Experiência</div> 
+            </div>
+        );
+    }
+}
